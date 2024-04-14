c3986dacca7ea92f882299c41d06dc421671a694
celneo7 <celesteneo46@yahoo.com.sg>
2024-04-14 18:50:05 +0800

    Add more form elements

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,95 +1,157 @@
-import React from "react"
-import InputFile from "./InputFile"
-import Dropdown from "./Dropdown"
+import React from "react";
+import InputFile from "./InputFile";
+import UploadType from "./UploadType";
 
-import axios from "axios"
+import axios from "axios";
+import QnNum from "./QnNum";
 
+export default function Input() {
+  const [opt, setOpt] = React.useState("");
 
-export default function Input(){
+  function handleOptChange(event) {
+    setOpt(event.target.value);
+  }
 
-    const [opt, setOpt] = React.useState("")
+  const [mcq, setMcq] = React.useState();
+  const [oe, setOe] = React.useState();
 
-    function handleOptChange(event){
-        setOpt(event.target.value)
+  const [error, setError] = React.useState({
+    mcq: false,
+    oe: false,
+  });
+
+  function handleDecrease(event) {
+    event.target.id === "mcq" ? mcq === "" && setMcq(0) : oe === "" && setOe(0);
+
+    event.target.id === "mcq"
+      ? 0 < mcq && setMcq((prev) => prev - 1)
+      : 0 < oe && setOe((prev) => prev - 1);
+  }
+
+  function handleIncrease(event) {
+    event.target.id === "mcq" ? mcq === "" && setMcq(0) : oe === "" && setOe(0);
+
+    event.target.id === "mcq"
+      ? mcq < 20 && setMcq((prev) => prev + 1)
+      : oe < 20 && setOe((prev) => prev + 1);
+  }
+
+  function handleChange(event) {
+    const { id, value } = event.target;
+
+    const newValue = Number.isInteger(parseInt(value)) ? parseInt(value) : "";
+
+    0 <= newValue && newValue <= 20
+      ? setError((prev) => {
+          return { ...prev, [id]: false };
+        })
+      : setError((prev) => {
+          return { ...prev, [id]: true };
+        });
+
+    console.log(error);
+    id === "mcq" ? setMcq(newValue) : setOe(newValue);
+  }
+
+  const [files, setFile] = React.useState([]);
+
+  function handleFile(event) {
+    const uploadedFiles = event.target.files;
+    setFile([...files, ...uploadedFiles]);
+  }
+
+  function handleDelete(index) {
+    setFile(files.toSpliced(index, 1));
+  }
+
+  const sendPDFToBackend = async () => {
+    try {
+      if (!files) {
+        console.log("no files");
+      }
+
+      const fd = new FormData();
+      files.forEach((file, index) => {
+        fd.append(`file${index + 1}`, file);
+      });
+
+      const response = await axios.post(
+        `http://localhost:5001/questions?module-code=DSA4213&input-type=${opt}&open-ended-count=${oe}&mcq-count=${mcq}`,
+        fd,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          responseType: "blob",
+        }
+      );
+
+      const blob = new Blob([response.data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+
+      // Create a temporary URL to the Excel file
+      const url = window.URL.createObjectURL(blob);
+
+      // Create a link and trigger the download
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "output.xlsx");
+      document.body.appendChild(link);
+      link.click();
+
+      console.log("PDF uploaded successfully:", response.data);
+    } catch (error) {
+      console.error("Error uploading PDF:", error);
     }
+  };
 
-    const [files, setFile] = React.useState([])
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    function handleFile(event){
-        const uploadedFiles = event.target.files;
-        setFile([...files, ...uploadedFiles])
-    }
+    sendPDFToBackend();
+  }
 
-    function handleDelete(index){
-        setFile(files.toSpliced(index, 1))
-    }
+  return (
+    <form onSubmit={handleSubmit} className="input-body">
+      <h1 className="file--title">
+        Upload slides or documents of the content you teach to generate Exam
+        Q&As!
+      </h1>
 
+      <div>
+        <span className="module">NUS Module:</span>
+        <select className="btn btn-secondary dropdown-toggle">
+          <option>--Choose Module--</option>
+          <option>DSA4213</option>
+          <option>DSA2102</option>
+          <option>DSA1101</option>
+        </select>
+      </div>
 
-    const sendPDFToBackend = async () => {
-        try {
-            if (!files){
-                console.log("no files")
-            }
-    
-            const fd = new FormData()
-            files.forEach((file, index) => {
-                fd.append(`file${index+1}`, file)
-            })
-    
-            const response = await axios.post('http://localhost:5001/questions', fd, {
-                headers: {
-                'Content-Type': 'multipart/form-data',
-                },
-                responseType:'blob'
-            });
+      <div className="input-grid">
+        <InputFile upload={handleFile} delete={handleDelete} files={files} />
 
-            const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-            
-            // Create a temporary URL to the Excel file 
-            const url = window.URL.createObjectURL(blob); 
-        
-            // Create a link and trigger the download 
-            const link = document.createElement('a'); 
-            link.href = url; 
-            link.setAttribute('download', 'output.xlsx'); 
-            document.body.appendChild(link); 
-            link.click(); 
+        <div class="buttons">
+          <UploadType value={opt} onChange={handleOptChange} />
+          <QnNum
+            mcq={mcq}
+            oe={oe}
+            increase={handleIncrease}
+            decrease={handleDecrease}
+            onChange={handleChange}
+            error={error}
+          />
 
-        
-            console.log('PDF uploaded successfully:', response.data);
-            } catch (error) {
-            console.error('Error uploading PDF:', error);
-            }
-      };
-    
-    function handleSubmit(event){
-
-        event.preventDefault();
-
-        sendPDFToBackend();
-    }
-
-    return(
-
-        <form onSubmit={handleSubmit} className="input-body">
-            <h1 className="file--title">Upload slides or documents of the content you teach to generate Exam Q&As!</h1>
-            
-            <div className="input-grid">
-                <InputFile 
-                    upload={handleFile}
-                    delete= {handleDelete}
-                    files = {files}/>
-        
-                <div class="buttons">                    
-                    <Dropdown
-                        value={opt}
-                        onChange={handleOptChange}/>
-                    
-                    <button className="btn btn-primary" value="Submit" onChange={handleSubmit}>
-                        Submit
-                    </button>       
-                </div>
-            </div>
-        </form>
-    )
+          <button
+            className="btn btn-primary"
+            value="Submit"
+            onChange={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
--- client/src/components/InputFile.js
+++ client/src/components/InputFile.js
@@ -1,44 +1,39 @@
-import React from "react"
+import React from "react";
 
-export default function InputFile(props){
-    return(
+export default function InputFile(props) {
+  return (
+    <div>
+      <div class="file">
         <div>
-            
+          {props.files.length === 0 && <p>Supported file types: .pdf, .doc</p>}
+          <label
+            htmlFor="formFileMultiple"
+            className={props.files.length === 0 ? "form-label" : "add"}
+          >
+            {props.files.length === 0 ? "Browse files" : "Add files"}
+          </label>
+          <input
+            type="file"
+            id="formFileMultiple"
+            multiple
+            onChange={props.upload}
+            name="file"
+          />
+          <div>
+            {props.files.map((file, index) => {
+              return (
+                <div className="delete" key={index}>
+                  <span>{file.name}</span>
 
-            <div class="file">
-                <div>
-                    {props.files.length === 0 && <p>Supported file types: .pdf, .doc</p>}
-                    <label 
-                        htmlFor="formFileMultiple" 
-                        className= {props.files.length === 0 ? "form-label" : "add"} >
-                        {props.files.length === 0 ? "Browse files" : "Add files"}
-                    </label>
-                    <input 
-                        type="file" 
-                        id="formFileMultiple" 
-                        multiple
-                        onChange={props.upload}
-                        name="file"/>
-                    <div>
-                        {props.files.map((file, index) =>{
-                            return(
-                            <div className="delete" key={index}>
-                                <span>{file.name}</span>
-
-                                <button 
-                                    type= "button" 
-                                    onClick={() => props.delete(index)}
-                                    >
-                                X
-                                </button>
-                            </div>
-                            )
-                        })}
-                    </div>
-
+                  <button type="button" onClick={() => props.delete(index)}>
+                    X
+                  </button>
                 </div>
-            </div>
+              );
+            })}
+          </div>
         </div>
-        
-    )
+      </div>
+    </div>
+  );
 }
