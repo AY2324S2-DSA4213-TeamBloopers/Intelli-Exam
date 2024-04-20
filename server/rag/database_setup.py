8458a34bf57abb21b197a803612f905389bcc1a8
leezhanpeng <zhanpenglee@outlook.com>
2024-04-21 01:07:43 +0800

    Clean up code

--- server/rag/database_setup.py
+++ server/rag/database_setup.py
@@ -1,44 +1,49 @@
 """
-Script for initialising a MongoDB database with course content.
+Script for initialising a MongoDB database with DSA4213 course content.
 Please note: This code should not be run again and is kept for tracking purposes.
 """
 
 from dotenv import load_dotenv
+from pdfminer.high_level import extract_text
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from sentence_transformers import SentenceTransformer
 
 import certifi
 import os
-import PyPDF2
 
 embedding_model = SentenceTransformer("thenlper/gte-large")
 
-def read_pdf(file_path):
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)     
-        page = reader.pages[0]
-        text = page.extract_text()
-        while text[:7] != "Summary":
-            text = text[1:]
-        return text[7:]
+def read_pdf():
+    texts = []
+
+    # For every lecture notes, extract content in chunks, remove common gibberish of symbols. Not perfectly extracted, but sufficient for the project.
+    for i in range(2, 8):
+        k = extract_text(f"rag/database_data/DSA4213-Lecture{i}.pdf").strip().replace("!\"#$%&’()*+\",-+($.-/\"0.(12’3(+&\"$4\"5(.)-%$’26\"7//\"8()*+3\"8232’1296\"", "").split("\n\n")
+        j = 0
+        text = ""
+        for i in k:
+            text += i
+            if i == "":
+                j += 1
+            if j == 10:
+                texts.append(text)
+                text = ""
+                j = 0
+        texts.append(text)
+    return texts
 
 def initialise_database():
-    pdf_values = [str(i) if len(str(i)) == 2 else "0" + str(i) for i in range(1, 11)]
-    all_data = []
-    for pdf_value in pdf_values:
-        text = read_pdf(f'/database_data/L{pdf_value}_outline.pdf').replace("\no", "\n\to").strip()
-
-        for i in text.split("\n \n"):
-            title, content = i.split("\n", 1)
-            all_data.append({"title": title.strip().replace(" -", "-"), "content": content, "embedding": embedding_model.encode(content).tolist()})
+    texts = read_pdf()
 
     load_dotenv()
-
     URI = os.getenv("RAG_DATABASE_URI")
     client = MongoClient(URI, server_api=ServerApi("1"), tlsCAFile=certifi.where())
-    collection = client["computer_vision"]["content"]
-    collection.insert_many(all_data)
+    collection = client["DSA4213"]["content"]
+
+    embeddings = list(map(lambda x: embedding_model.encode(x).tolist(), texts))
+    docs = [{"content": content, "embedding": embedding} for content, embedding in zip(texts, embeddings)]
+    collection.insert_many(docs)
 
 if __name__ == "__main__":
     initialise_database()
